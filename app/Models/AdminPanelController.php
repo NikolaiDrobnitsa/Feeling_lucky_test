595374c349da736a854fbac13ecd412d6fc97223
Nikolai <Drob_mf41@student.itstep.org>
2023-08-18 03:31:34 +0300

    add controller

---
+++ app/Models/AdminPanelController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AdminPanelController extends Model
+{
+    use HasFactory;
+}
