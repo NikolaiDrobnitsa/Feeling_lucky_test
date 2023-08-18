4508be8934b39dd86d1688bc11a218634ee3e214
Nikolai <Drob_mf41@student.itstep.org>
2023-08-18 03:43:42 +0300

    add correct controller and model

--- app/Models/UniqueLink.php
+++ app/Models/UniqueLink.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UniqueLinkController extends Model
+class UniqueLink extends Model
 {
     use HasFactory;
+    protected $fillable = ['link'];
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -19,27 +19,13 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
-        'password',
+        'phone_number',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    public function uniqueLinks()
+    {
+        return $this->hasMany(UniqueLink::class);
+    }
+
 }
